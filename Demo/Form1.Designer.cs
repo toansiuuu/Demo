4252777f46d4bb3808e47b3d6847f8a9831415d7
toanhominh142@gmail.com <toanhominh142@gmail.com>
2023-10-20 08:41:11 +0700

    sửa

--- Demo/Form1.Designer.cs
+++ Demo/Form1.Designer.cs
@@ -28,12 +28,35 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            bindingSource1 = new BindingSource(components);
+            domainUpDown1 = new DomainUpDown();
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).BeginInit();
+            SuspendLayout();
+            // 
+            // domainUpDown1
+            // 
+            domainUpDown1.Location = new Point(282, 152);
+            domainUpDown1.Name = "domainUpDown1";
+            domainUpDown1.Size = new Size(150, 27);
+            domainUpDown1.TabIndex = 0;
+            domainUpDown1.Text = "domainUpDown1";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(domainUpDown1);
+            Name = "Form1";
+            Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)bindingSource1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private BindingSource bindingSource1;
+        private DomainUpDown domainUpDown1;
     }
 }
